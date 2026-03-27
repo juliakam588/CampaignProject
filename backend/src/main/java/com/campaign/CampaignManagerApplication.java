981dc55eda1dab0ac75473d4f07c9a4915d7363b
juliakam588 <julka.kamuda@gmail.com>
2026-03-27 18:41:23 +0100

    feat: start spring boot project with h2 configuration

---
+++ backend/src/main/java/com/campaign/CampaignManagerApplication.java
@@ -0,0 +1,12 @@
+package com.campaign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampaignManagerApplication.class, args);
+    }
+}
